acf8339902ee6d18cdb3b4a28288e8721d7d3e44
Christopher "chief" Najewicz <chief@beefdisciple.com>
2023-03-23 16:10:03 -0400

    revert to older MW install

--- mediawiki/bin/install_extensions.sh
+++ mediawiki/bin/install_extensions.sh
@@ -5,13 +5,13 @@ set -e
 : ${EXT_DIR:-extensions}
 
 extensions=(
-  https://extdist.wmflabs.org/dist/extensions/MobileFrontend-REL1_39-12efce7.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/YouTube-REL1_39-f272bb3.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/MultimediaViewer-REL1_39-c311f4e.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/RandomImage-REL1_39-a3cccf7.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/CategoryTree-REL1_39-9e6b7c8.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/ParserFunctions-REL1_39-5cdead8.tar.gz
-  https://extdist.wmflabs.org/dist/extensions/MsUpload-REL1_39-f4941a8.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/MobileFrontend-REL1_31-7f66849.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/YouTube-REL1_31-f6252db.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/MultimediaViewer-REL1_31-1273d3e.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/RandomImage-REL1_31-a1b5400.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/CategoryTree-REL1_32-5866bb9.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/ParserFunctions-REL1_32-15fe1aa.tar.gz
+  https://extdist.wmflabs.org/dist/extensions/MsUpload-REL1_32-0779791.tar.gz
 )
 
 for ext_url in ${extensions[@]}; do
